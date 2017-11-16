dbbbfbe0ebfae9289a7e201c3485a3a060435691
DanielDixon324 <32543917+DanielDixon324@users.noreply.github.com>
2017-11-16 12:46:55 +0000

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,12 @@
+def Test1():
+	x = 35
+	y = "hello"
+	if x == 35 and y == "hello":
+		x = x + 1
+		y = y + " world"
+	print (x)
+	print (y)
+	Name = input("What is your name? ")
+	print (Name)
+	
+Test1()
