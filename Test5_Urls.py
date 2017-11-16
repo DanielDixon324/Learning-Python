3e727c11dbd1b0b52cda857f8542c605a6c457d1
DanielDixon324 <32543917+DanielDixon324@users.noreply.github.com>
2017-11-16 12:58:50 +0000

    Create Test5_Urls.py

---
+++ Test5_Urls.py
@@ -0,0 +1,6 @@
+#test.py
+import sys
+import requests
+req = requests.get('http://resources.infosecinstitute.com/')
+print('Response Code: ' + str(req.status_code))
+print('\nResponse:\n' + req.text)
